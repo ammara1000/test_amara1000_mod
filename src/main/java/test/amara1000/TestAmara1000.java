01038abc516bba56610ff3f4fe7aa223b121c180
amara1000 <ammara1000@outlook.fr>
2024-11-16 20:43:22 +0100

    yo

--- src/main/java/test/amara1000/TestAmara1000.java
+++ src/main/java/test/amara1000/TestAmara1000.java
@@ -4,6 +4,9 @@ import net.fabricmc.api.ModInitializer;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import test.amara1000.item.ModItems;
+
+import static test.amara1000.item.ModItems.*;
 
 public class TestAmara1000 implements ModInitializer {
 	public static final String MOD_ID = "test_amara1000";
@@ -11,6 +14,7 @@ public class TestAmara1000 implements ModInitializer {
 
 	@Override
 	public void onInitialize() {
+		ModItems.registerModItems();
 		LOGGER.info("Hello Fabric world!");
 	}
 }
---
+++ src/main/java/test/amara1000/item/ModItems.java
@@ -0,0 +1,31 @@
+package test.amara1000.item;
+
+
+import net.fabricmc.fabric.api.itemgroup.v1.ItemGroupEvents;
+import net.minecraft.item.Item;
+import net.minecraft.item.ItemGroups;
+import net.minecraft.registry.Registries;
+import net.minecraft.registry.Registry;
+import net.minecraft.util.Identifier;
+import test.amara1000.TestAmara1000;
+
+public class ModItems {
+    public static final Item LOL = registerItem("lol", new Item(new Item.Settings()));
+
+
+
+
+
+    private static Item registerItem(String name, Item item){
+        return Registry.register(Registries.ITEM, Identifier.of(TestAmara1000.MOD_ID, name), item);
+    }
+
+
+    public static void registerModItems(){
+        TestAmara1000.LOGGER.info("Registering Mod Items for "+TestAmara1000.MOD_ID);
+
+        ItemGroupEvents.modifyEntriesEvent(ItemGroups.INGREDIENTS).register(entries -> {
+            entries.add(LOL);
+        });
+    }
+}
