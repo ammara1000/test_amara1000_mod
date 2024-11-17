8d64a6304a73ff53a874e3b90cd8cfe8b767c901
amara1000 <ammara1000@outlook.fr>
2024-11-17 13:01:47 +0100

    yo

--- src/main/java/test/amara1000/block/ModBlocks.java
+++ src/main/java/test/amara1000/block/ModBlocks.java
@@ -1,9 +1,11 @@
 package test.amara1000.block;
 
+import net.fabricmc.fabric.api.itemgroup.v1.ItemGroupEvents;
 import net.minecraft.block.AbstractBlock;
 import net.minecraft.block.Block;
 import net.minecraft.item.BlockItem;
 import net.minecraft.item.Item;
+import net.minecraft.item.ItemGroups;
 import net.minecraft.registry.Registries;
 import net.minecraft.registry.Registry;
 import net.minecraft.sound.BlockSoundGroup;
@@ -13,8 +15,11 @@ import test.amara1000.TestAmara1000;
 public class ModBlocks {
 
     public static final Block LOL_BLOCK=registerBlock("lol_block",
-            new Block(AbstractBlock.Settings.create().strength(4f)
-                    .requiresTool().sounds(BlockSoundGroup.AMETHYST_BLOCK)));
+            new Block(AbstractBlock.Settings.create()
+                    .strength(0.5f)
+                    .requiresTool()
+                    .sounds(BlockSoundGroup.AMETHYST_BLOCK)
+            ));
 
     private static Block registerBlock(String name,Block block){
         registerBlockItem(name,block);
@@ -29,5 +34,9 @@ public class ModBlocks {
 
     public static void registerModBlocks(){
         TestAmara1000.LOGGER.info("Registering Mod Blocks for "+TestAmara1000.MOD_ID);
+
+        ItemGroupEvents.modifyEntriesEvent(ItemGroups.BUILDING_BLOCKS).register(entries -> {
+            entries.add(ModBlocks.LOL_BLOCK);
+        });
     }
 }
