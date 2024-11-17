4d8ea3efca8ad315817f991ad4a3bdb78ad7590b
amara1000 <ammara1000@outlook.fr>
2024-11-17 12:26:48 +0100

    yo

--- src/main/java/test/amara1000/TestAmara1000.java
+++ src/main/java/test/amara1000/TestAmara1000.java
@@ -4,6 +4,7 @@ import net.fabricmc.api.ModInitializer;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import test.amara1000.block.ModBlocks;
 import test.amara1000.item.ModItems;
 
 import static test.amara1000.item.ModItems.*;
@@ -15,6 +16,7 @@ public class TestAmara1000 implements ModInitializer {
 	@Override
 	public void onInitialize() {
 		ModItems.registerModItems();
+		ModBlocks.registerModBlocks();
 		LOGGER.info("Hello Fabric world!");
 	}
 }
---
+++ src/main/java/test/amara1000/block/ModBlocks.java
@@ -0,0 +1,33 @@
+package test.amara1000.block;
+
+import net.minecraft.block.AbstractBlock;
+import net.minecraft.block.Block;
+import net.minecraft.item.BlockItem;
+import net.minecraft.item.Item;
+import net.minecraft.registry.Registries;
+import net.minecraft.registry.Registry;
+import net.minecraft.sound.BlockSoundGroup;
+import net.minecraft.util.Identifier;
+import test.amara1000.TestAmara1000;
+
+public class ModBlocks {
+
+    public static final Block LOL_BLOCK=registerBlock("lol_block",
+            new Block(AbstractBlock.Settings.create().strength(4f)
+                    .requiresTool().sounds(BlockSoundGroup.AMETHYST_BLOCK)));
+
+    private static Block registerBlock(String name,Block block){
+        registerBlockItem(name,block);
+        return Registry.register(Registries.BLOCK, Identifier.of(TestAmara1000.MOD_ID,name),block);
+    }
+
+    private static void registerBlockItem(String name, Block block){
+        Registry.register(Registries.ITEM, Identifier.of(TestAmara1000.MOD_ID, name),
+                new BlockItem(block, new Item.Settings()));
+    }
+
+
+    public static void registerModBlocks(){
+        TestAmara1000.LOGGER.info("Registering Mod Blocks for "+TestAmara1000.MOD_ID);
+    }
+}
