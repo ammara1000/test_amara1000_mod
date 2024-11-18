f6b84b240e27b0db61b8497b3111857e57c3d566
amara1000 <ammara1000@outlook.fr>
2024-11-18 09:03:27 +0100

    yo

--- src/main/java/test/amara1000/TestAmara1000Client.java
+++ src/main/java/test/amara1000/TestAmara1000Client.java
@@ -1,10 +1,13 @@
 package test.amara1000;
 
 import net.fabricmc.api.ClientModInitializer;
+import net.fabricmc.fabric.api.blockrenderlayer.v1.BlockRenderLayerMap;
+import net.minecraft.block.Blocks;
+import net.minecraft.client.render.RenderLayer;
 
 public class TestAmara1000Client implements ClientModInitializer {
     @Override
     public void onInitializeClient() {
-
+        //BlockRenderLayerMap.INSTANCE.putBlock(Blocks.STONE, RenderLayer.getCutout());
     }
 }
